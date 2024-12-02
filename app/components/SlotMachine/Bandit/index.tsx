edbbab6d235e8b935901d8db959957570004c002
Zhurkin-Alex <jura-leha-999@mail.ru>
2024-12-02 12:27:19 +0300

    close menu if click

--- app/components/SlotMachine/Bandit/index.tsx
+++ app/components/SlotMachine/Bandit/index.tsx
@@ -1,13 +1,19 @@
 'use client'
 
+import { ACTION_TYPE, useAppContext } from '@/app/context/AppContext'
 import React from 'react'
 
 import SlotMachina from './components/SlotMachina'
 import { StyledBody, StyledWrapper } from './style'
 
 const Bandit = () => {
+  const { dispatch } = useAppContext()
+
+  const clickMenu = () => {
+    dispatch({ type: ACTION_TYPE.setOverlayClickCloseBurger, payload: true })
+  }
   return (
-    <StyledWrapper>
+    <StyledWrapper onClick={clickMenu}>
       <StyledBody>
         <SlotMachina />
       </StyledBody>
